a1450f569d0eae064f6c499b886574e770ccd9b5
Keloran <keloran@develbox.info>
2023-11-30 13:28:10 +0000

    hmm

--- internal/hooks/agent.go
+++ internal/hooks/agent.go
@@ -1,17 +1,10 @@
 package hooks
 
 import (
-  "context"
-  "encoding/json"
-  "fmt"
-  "github.com/bugfixes/go-bugfixes/logs"
-  ConfigBuilder "github.com/keloran/go-config"
-  "net/http"
-
-  keybuf "github.com/k8sdeploy/protos/generated/key/v1"
-  orcbuf "github.com/k8sdeploy/protos/generated/orchestrator/v1"
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/credentials/insecure"
+	"encoding/json"
+	"fmt"
+	ConfigBuilder "github.com/keloran/go-config"
+	"net/http"
 )
 
 type Hooks struct {
@@ -74,81 +67,81 @@ func (h *Hooks) ValidateKey(companyID, key, secret string) (bool, error) {
 		return true, nil
 	}
 
-	conn, err := grpc.Dial(h.Config.Local.KeyService.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return false, logs.Errorf("validateKey failed to dial key service: %v", err)
-	}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			fmt.Printf("failed to close connection: %v", err)
-		}
-	}()
-
-	c := keybuf.NewKeyServiceClient(conn)
-	resp, err := c.ValidateHookKey(context.Background(), &keybuf.ValidateSystemKeyRequest{
-		ServiceKey: h.Config.Local.KeyService.Key,
-		CompanyId:  companyID,
-		Key:        key,
-		Secret:     secret,
-	})
-	if err != nil {
-		return false, logs.Errorf("validateKey failed to validate key: %v", err)
-	}
-
-	if resp.Status != nil {
-		return false, logs.Errorf("unknown status: %v", resp.Status)
-	}
-
-	if resp.Valid {
-		return true, nil
-	}
+	//conn, err := grpc.Dial(h.Config.Local.KeyService.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//if err != nil {
+	//	return false, logs.Errorf("validateKey failed to dial key service: %v", err)
+	//}
+	//defer func() {
+	//	if err := conn.Close(); err != nil {
+	//		fmt.Printf("failed to close connection: %v", err)
+	//	}
+	//}()
+	//
+	//c := keybuf.NewKeyServiceClient(conn)
+	//resp, err := c.ValidateHookKey(context.Background(), &keybuf.ValidateSystemKeyRequest{
+	//	ServiceKey: h.Config.Local.KeyService.Key,
+	//	CompanyId:  companyID,
+	//	Key:        key,
+	//	Secret:     secret,
+	//})
+	//if err != nil {
+	//	return false, logs.Errorf("validateKey failed to validate key: %v", err)
+	//}
+	//
+	//if resp.Status != nil {
+	//	return false, logs.Errorf("unknown status: %v", resp.Status)
+	//}
+	//
+	//if resp.Valid {
+	//	return true, nil
+	//}
 
 	return false, nil
 }
 
 func (h *Hooks) InformOrchestrator(i HookEvent, companyID, key, secret string) error {
-	conn, err := grpc.Dial(h.Config.Local.OrchestratorService.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return logs.Errorf("InformOrchestrator failed to dial orchestrator service: %v", err)
-	}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			_ = logs.Errorf("failed to close connection: %v", err)
-		}
-	}()
-
-	ob := &orcbuf.DeploymentRequest{
-		ServiceKey: h.Config.Local.OrchestratorService.Key,
-		K8SDetails: &orcbuf.K8SDetails{
-			ImageHash:        i.ImageHash,
-			ImageTag:         i.ImageTag,
-			ServiceName:      i.ServiceName,
-			ServiceNamespace: i.ServiceNamespace,
-			HookDetails: &orcbuf.HookDetails{
-				Id:     companyID,
-				Key:    key,
-				Secret: secret,
-			},
-		},
-	}
-
-	if i.GithubEvent.GithubCommits != nil {
-		ob.AuthorDetails = &orcbuf.AuthorDetails{
-			Username: i.GithubEvent.GithubCommits[0].Author.Username,
-			Name:     i.GithubEvent.GithubCommits[0].Author.Name,
-			Email:    i.GithubEvent.GithubCommits[0].Author.Email,
-		}
-		ob.CommitMessage = &i.GithubEvent.GithubCommits[0].Message
-		ob.CommitId = &i.GithubEvent.GithubCommits[0].ID
-		ob.CommitUrl = &i.GithubEvent.GithubCommits[0].URL
-	}
-
-	c := orcbuf.NewOrchestratorClient(conn)
-	resp, err := c.Deploy(context.Background(), ob)
-	if err != nil {
-		return logs.Errorf("InformOrchestrator failed to inform orchestrator: %v", err)
-	}
-	fmt.Printf("InformOrchestrator response: %+v\n", resp)
+	//conn, err := grpc.Dial(h.Config.Local.OrchestratorService.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//if err != nil {
+	//	return logs.Errorf("InformOrchestrator failed to dial orchestrator service: %v", err)
+	//}
+	//defer func() {
+	//	if err := conn.Close(); err != nil {
+	//		_ = logs.Errorf("failed to close connection: %v", err)
+	//	}
+	//}()
+	//
+	//ob := &orcbuf.DeploymentRequest{
+	//	ServiceKey: h.Config.Local.OrchestratorService.Key,
+	//	K8SDetails: &orcbuf.K8SDetails{
+	//		ImageHash:        i.ImageHash,
+	//		ImageTag:         i.ImageTag,
+	//		ServiceName:      i.ServiceName,
+	//		ServiceNamespace: i.ServiceNamespace,
+	//		HookDetails: &orcbuf.HookDetails{
+	//			Id:     companyID,
+	//			Key:    key,
+	//			Secret: secret,
+	//		},
+	//	},
+	//}
+	//
+	//if i.GithubEvent.GithubCommits != nil {
+	//	ob.AuthorDetails = &orcbuf.AuthorDetails{
+	//		Username: i.GithubEvent.GithubCommits[0].Author.Username,
+	//		Name:     i.GithubEvent.GithubCommits[0].Author.Name,
+	//		Email:    i.GithubEvent.GithubCommits[0].Author.Email,
+	//	}
+	//	ob.CommitMessage = &i.GithubEvent.GithubCommits[0].Message
+	//	ob.CommitId = &i.GithubEvent.GithubCommits[0].ID
+	//	ob.CommitUrl = &i.GithubEvent.GithubCommits[0].URL
+	//}
+	//
+	//c := orcbuf.NewOrchestratorClient(conn)
+	//resp, err := c.Deploy(context.Background(), ob)
+	//if err != nil {
+	//	return logs.Errorf("InformOrchestrator failed to inform orchestrator: %v", err)
+	//}
+	//fmt.Printf("InformOrchestrator response: %+v\n", resp)
 
 	return nil
 }
